f288d20c782453d29482463393f25bf3e0d277d6
nurlight05 <nurlight05@gmail.com>
2022-08-08 13:22:44 +0600

    Settings file changes.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
