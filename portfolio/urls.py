9c04e23010d29ae7fe3ccc83cbf1b0a2025ae412
nurlight05 <nurlight05@gmail.com>
2022-11-25 00:54:45 +0500

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from blog.views import showText
+
 urlpatterns = [
     path('', include('landing.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path(r'competition/', showText, name="show_text")
 ]
 
 if settings.DEBUG:
